41fc080ad53836c02aa84ef6db4bf6b3d6232f50
JohnGarden <jrdmnt@gmail.com>
2016-10-02 00:51:04 -0300

    Add mongoDB

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET ShopList page. */
+router.get('/shoplist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('shopcollection');
+    collection.find({},{},function(e,docs){
+        res.render('shoplist', {
+            "shoplist" : docs
+        });
+    });
+});
+
 module.exports = router;
