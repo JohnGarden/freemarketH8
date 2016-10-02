2aaee44e1ebb3d3451aa140d77f811838a995f44
felipeViana <felipe.140492@gmail.com>
2016-10-01 23:47:28 -0300

    Reverts "Initializing shop page"
    This reverts commit c18c37dd943a123c5a2e7e5353270514861b4cf0.

--- routes/lojas.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET lojas page. */
-router.get('/', function(req, res, next) {
-  res.render('lojas', { title: 'Lojas H8' });
-});
-
-module.exports = router;
