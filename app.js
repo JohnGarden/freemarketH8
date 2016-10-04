19592dfb96c18cb92d32fcf765504f5910e43d19
Victor Elias <victorgelias@gmail.com>
2016-10-04 15:09:28 -0300

    Refactoring shops router source files
    shoplist -> shops
    unifying shop list and shops page into shops.js router

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var db = monk('localhost:27017/freemarketh8');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var shoplist = require('./routes/shoplist');
+var shops = require('./routes/shops');
 
 var app = express();
 
@@ -35,7 +35,7 @@ app.use(function(req,res,next){
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/shoplist', shoplist);
+app.use('/shops', shops);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-/* GET ShopList page. */
-router.get('/shoplist', function(req, res) {
-    var db = req.db;
-    var collection = db.get('shopcollection');
-    collection.find({},{},function(e,docs){
-        res.render('shoplist', {
-            "shoplist" : docs
-        });
-    });
+  res.redirect('/shops');
 });
 
 module.exports = router;
